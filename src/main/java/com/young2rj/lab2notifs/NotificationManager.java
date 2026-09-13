f68078967aab49b8e4f2cd4ed09c8ea0df9a21be
RivalJ <157822657+RivalJ@users.noreply.github.com>
2026-09-13 18:01:57 -0400

    Updated for testing

--- src/main/java/com/young2rj/lab2notifs/NotificationManager.java
+++ src/main/java/com/young2rj/lab2notifs/NotificationManager.java
@@ -2,9 +2,10 @@ package com.young2rj.lab2notifs;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+@Service
 public class NotificationManager {
     NotifcationService service;
-    public NotificationManager(@Qualifier("email") NotifcationService service) {
+    public NotificationManager(@Qualifier("SMSNotificationService") NotifcationService service) {
         this.service = service;
     }
     public void sendNotification(String message) {
